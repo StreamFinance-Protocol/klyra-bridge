1282c7e5710fffd6046a7cfad9868d85a9b91e3a
0xDro <alegala21@gmail.com>
2025-01-09 16:35:10 -0800

    [BRIDGE-1] Add base deposit func

---
+++ src/Bridge.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.8.0;
+import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+contract Brdige is ReentrancyGuard {
+    using SafeERC20 for IERC20;
+
+    uint256 public currId;
+    IERC20 public sdai;
+
+    event Bridge(
+        uint256 indexed id,
+        uint256 amount,
+        address from,
+        bytes toAddress
+    );
+
+    constructor(address _sdai) {
+        require(_sdai != address(0), "Invalid address");
+        sdai = IERC20(_sdai);
+    }
+    function deposit(
+        uint256 amount,
+        bytes calldata toAddress
+    ) public nonReentrant {
+        require(amount > 0, "Cannot bridge zero");
+        require(toAddress.length != 0, "Invalid address");
+
+        sdai.safeTransferFrom(msg.sender, address(this), amount);
+
+        emit Bridge(currId++, amount, msg.sender, toAddress);
+    }
+}
