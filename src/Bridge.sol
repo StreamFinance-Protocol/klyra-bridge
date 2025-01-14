86c09632fa2891abf97c83ff170d623dffc5abdc
ea-coding <92234415+ea-coding@users.noreply.github.com>
2025-01-13 21:06:14 -0800

    [BRIDGE-2] Add owner to contract

--- src/Bridge.sol
+++ src/Bridge.sol
@@ -1,9 +1,10 @@
 pragma solidity ^0.8.0;
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-contract Brdige is ReentrancyGuard {
+contract Bridge is ReentrancyGuard {
     using SafeERC20 for IERC20;
 
+    address public owner;
     uint256 public currId;
     IERC20 public sdai;
 
@@ -17,7 +18,14 @@ contract Brdige is ReentrancyGuard {
     constructor(address _sdai) {
         require(_sdai != address(0), "Invalid address");
         sdai = IERC20(_sdai);
+        owner = msg.sender;
     }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the contract owner");
+        _;
+    }
+
     function deposit(
         uint256 amount,
         bytes calldata toAddress
